a63f0b94c3fa9ed0c76df945bdcbf6984241b8be
Remi-Avec-Un-I <74991151+Remi-Avec-Un-I@users.noreply.github.com>
2022-02-14 21:55:15 +0100

    Update majdetect.py

--- majdetect.py
+++ majdetect.py
@@ -1,10 +1,11 @@
-from infi.systray import SysTrayIcon    #pip install infi.systray
+from infi.systray import SysTrayIcon    #pip install infi.systray       https://github.com/Infinidat/infi.systray
 from win32api import GetKeyState, GetAsyncKeyState
 from win32con import VK_LSHIFT, VK_RSHIFT, VK_CAPITAL
+import ctypes
 import time
 
 def majdetector(systray):
-    print("Hello, World!")
+    ctypes.windll.user32.MessageBoxW(None, u"The icon become red when you are in capital charaters", u"About", 0)
 
 menu_options = (("Detecteur de Maj", None, majdetector),)
 systray = SysTrayIcon("vert.ico", "Detecteur de maj", menu_options)
